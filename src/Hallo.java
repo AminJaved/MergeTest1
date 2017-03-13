cbf4a1f8cda08e1a844477ea320ecd485b9fc9e7
Amin Javed <ajaved@student.tgm.ac.at>
2017-03-13 10:57:10 +0100

    Merge

--- src/Hallo.java
+++ src/Hallo.java
@@ -2,19 +2,11 @@
 public class Hallo {
 	
 	public static void gutenTag(){
-<<<<<<< HEAD
 		System.out.println("Guten Tag....");
-=======
-		System.out.println("Guten Tag.!!!");
->>>>>>> refs/remotes/merge1/master
 	}
 
 	public static void byby(){
-<<<<<<< HEAD
 		System.out.println("Auf Wiedersehen....");
-=======
-		System.out.println("Auf Wiedersehen.!!!");
->>>>>>> refs/remotes/merge1/master
 	}
 	
 	public static void main(String[] args) {
